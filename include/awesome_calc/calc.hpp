a6cc139858d3ebc42c9772cb0cc04dc5baaf6b94
Mand <pddMas@mail.ru>
2025-12-04 02:42:12 +0300

    Fix Meson build on Windows v2

--- include/awesome_calc/calc.hpp
+++ include/awesome_calc/calc.hpp
@@ -5,10 +5,21 @@
 
 namespace awesome_calc {
 
+#if defined(_WIN32) && defined(AWESOME_CALC_SHARED)
+// На Windows для DLL требуется явный экспорт/импорт символов; на static/*nix макрос пустой.
+#if defined(AWESOME_CALC_BUILD)
+#define AWESOME_CALC_API __declspec(dllexport)
+#else
+#define AWESOME_CALC_API __declspec(dllimport)
+#endif
+#else
+#define AWESOME_CALC_API
+#endif
+
 // Складывает два целых числа без побочных эффектов.
-int add(int a, int b);
+AWESOME_CALC_API int add(int a, int b);
 
 // Среднее арифметическое; бросает std::invalid_argument при пустом векторе.
-double mean(const std::vector<double> &values);
+AWESOME_CALC_API double mean(const std::vector<double> &values);
 
 } // namespace awesome_calc
