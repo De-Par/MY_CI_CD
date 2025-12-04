c715a93f72ca00bffb00cdfda4696f215271924d
Mand <pddMas@mail.ru>
2025-12-04 05:20:47 +0300

    Format code for clang-format v5

--- tests/unit/calc_unit_test.cpp
+++ tests/unit/calc_unit_test.cpp
@@ -54,13 +54,11 @@ TEST_P(ClampAddTest, SaturatesWithinBounds) {
     EXPECT_EQ(awesome_calc::clamp_add(a, b, mn, mx), expected);
 }
 
-INSTANTIATE_TEST_SUITE_P(
-    ClampCases,
-    ClampAddTest,
-    ::testing::Values(ClampParams{1, 2, -10, 10, 3},         // внутри диапазона
-                      ClampParams{100, 50, -10, 120, 120},   // верхняя насыщенность
-                      ClampParams{-100, -50, -120, 10, -120}, // нижняя насыщенность
-                      ClampParams{0, 0, -1, 1, 0}));         // на границе
+INSTANTIATE_TEST_SUITE_P(ClampCases, ClampAddTest,
+                         ::testing::Values(ClampParams{1, 2, -10, 10, 3},
+                                           ClampParams{100, 50, -10, 120, 120},
+                                           ClampParams{-100, -50, -120, 10, -120},
+                                           ClampParams{0, 0, -1, 1, 0}));
 
 TEST(ClampAddTest, ThrowsOnInvalidBounds) {
     EXPECT_THROW(awesome_calc::clamp_add(1, 2, 10, -10), std::invalid_argument);
